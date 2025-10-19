a47261b778fa01709cf11253401df17c759b3291
Maxiethehatter <maxnelson90@gmail.com>
2025-10-19 17:56:29 +0200

    Initial Commit

---
+++ DiceRollerApp/DiceRollerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerAppApp.swift
+//  DiceRollerApp
+//
+//  Created by Max Nelson on 2025-10-19.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
